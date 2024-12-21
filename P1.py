4025eb37f9c202dfd05b89ccf84dcf2932a1f282
sarangabani1984 <sarangabani1984@gmail.com>
2024-12-21 12:02:57 +0530

    c1

---
+++ P1.py
@@ -0,0 +1,13 @@
+print("@@@@@@@@@@@@ all is well @@@@@@@@@@")
+
+user = input("hi there how are you doing today:? shall we play game (Y/N)").lower()
+
+if user == "y":
+    print("great sure what you want play ")
+elif user == "n":
+    print("cool.. will play next time")
+else:
+    print(" you have entered invalid input, pls select y or n")
+    
+
+
