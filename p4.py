355bc452b70c7c0831abd4994fae3fc474d0f3d7
sarangabani1984 <151030032+sarangabani1984@users.noreply.github.com>
2024-12-24 15:45:21 +0530

    pyramind

---
+++ p4.py
@@ -0,0 +1,12 @@
+
+# def pattern(n):
+#   for i in range(1,n+1):
+#     print(" " * (n-i) + "@" * (2*i-1))
+#     print("\n")
+
+
+# pattern(10)
+
+for i in range(1,5):
+    print( "@" * (2*i-1)+" " * i)
+    print("\n")
